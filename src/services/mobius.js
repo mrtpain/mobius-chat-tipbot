922f9606e5253f78bddf46704aab10ebef241085
Gleb Pospelov <codefo@codefo.com>
2017-12-06 18:40:34 +0300

    Added MOBIUS_API_HOST option

--- src/services/mobius.js
+++ src/services/mobius.js
@@ -2,6 +2,7 @@ const Mobius = require('@mobius-network/mobius-node');
 const config = require('../config');
 
 const mobius = new Mobius({
+  host: config.MOBIUS_API_HOST,
   apiKey: config.MOBIUS_API_KEY,
 });
 
